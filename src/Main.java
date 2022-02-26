9aedf186cf24cb673b9993aef6557746343e2f8a
majbc1999 <majbc1999@gmail.com>
2022-02-26 15:55:00 +0100

    move up added to game

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import basic.Game;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Game igra = new Game(3);
-        igra.SpawnRandomNumber();
+        Game igra = new Game(4);
+        
+        for (int i = 0; i < 11; i++) {
+            igra.spawnRandomNumber();
+            Thread.sleep(200);
+        }
+
+        igra.print();
+
+        igra.moveUp();
+        System.out.println("-----------");
+        System.out.println("Moving up");
+        System.out.println("-----------");
         igra.print();
     }
 }
--- src/basic/Game.java
+++ src/basic/Game.java
@@ -1,6 +1,7 @@
 // game logic class
 package basic;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Game {
@@ -18,9 +19,10 @@ public class Game {
     }
 
     // function that spawns a 2 or 4 in empty place on 
-    public void SpawnRandomNumber() {
+    public void spawnRandomNumber() {
         int newNumber;
         int numberOfEmptySpaces = 0;
+        
         Random random = new Random(System.currentTimeMillis());
 
         // we imply that 2 is spawned with 90% probability and 4 with 10% probability
@@ -51,6 +53,58 @@ public class Game {
                         this.board[i][j] = newNumber;
                     }
                     numberOfEmptySpaces++;
+                }
+            }
+        }
+    }
+
+    // method that moves numbers up
+    public void moveUp() {
+        ArrayList<Integer> emptyIndexes = new ArrayList<Integer>();
+        boolean moveDone = false;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                // empty element in the grid
+                int number = board[j][i];
+                if (number != 0) {
+                    // we search for same number or empty space above
+                    for (int k = 1; k < j + 1; k++) {
+                        // same number above
+                        if (board[j - k][i] == number) {
+                            board[j - k][i] = 2 * number + 1;
+                            board[j][i] = 0;
+                            moveDone = true;
+                            break;
+                        }
+                        // other number above
+                        else if (board[j - k][i] != number && board[j - k][i] != 0) {
+                            break;
+                        }
+                        // empty space above
+                        else if (board[j - k][i] == 0) {
+                            emptyIndexes.add(j - k); 
+                        }
+                    }
+
+                    // we move the number in the furthest empty spot
+                    if (!moveDone) {
+                        if (emptyIndexes.size() != 0) {
+                            board[emptyIndexes.get(emptyIndexes.size() - 1)][i] = number;
+                            board[j][i] = 0;
+                        }
+                    }
+                    else {
+                        moveDone = false;
+                        emptyIndexes.clear();
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] % 2 != 0) {
+                    board[i][j] = board[i][j] - 1;
                 }
             }
         }
